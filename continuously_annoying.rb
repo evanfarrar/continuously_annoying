148ed97b6753bcbaae1be92b9be9778b53d75949
Evan Farrar <evanfarrar@gmail.com>
2009-05-18 16:27:19 -0500

    Dear cron, we're not friends anymore. Regards, Evan

--- continuously_annoying.rb
+++ continuously_annoying.rb
@@ -1,8 +1,11 @@
 %w{rubygems open-uri hpricot}.each{|g|require g}
 filename = "/dev/ttyUSB0"
 @f = File.open(filename, 'w+')
-if (Hpricot(open("http://ci.transis.net"))/"li.failed").any?
-  @f.putc ?g
-else
-  @f.putc ?s
+loop do
+  if (Hpricot(open("http://ci.transis.net"))/"li.failed").any?
+    @f.putc ?g
+  else
+    @f.putc ?s
+  end
+  sleep 60
 end
